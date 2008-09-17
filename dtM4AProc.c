99d973b0399501cb7da8f0436c1271d32215f36c
Ryan Joseph <ryan.joseph@gmail.com>
2008-09-17 00:13:05 -0700

    'stco' atom adjustments

--- dtM4AProc.c
+++ dtM4AProc.c
@@ -22,23 +22,33 @@ BOOL mogrifyAtomIntoFreeSpace(mpeg4atom_t* m4a, char* atomName)
 BOOL removeAtomFromMPEG4ForReals(mpeg4atom_t* m4a, char* atomName)
 {
 	mpeg4atom_t* atom = findAtomWithName(m4a, atomName);
+	mpeg4atom_t* stco = findAtomWithName(m4a, "stco");
 	
-	if (atom) {
+	if (atom && stco) {
 		mpeg4atom_t* parent = atom->parent;
 		
 		if (parent) {
 			parent->firstChild = atom->next;
 			
 			// adjust atom lengths for the parent atom, it's parent (atom's grandparent), until no parent is found
-			for (; parent; parent = parent->parent)
+			for (; parent; parent = parent->parent) {
+				printf("Adjusting length for atom 0x%x by -%d\n", parent, atom->length);
 				parent->length -= atom->length;
+			}
 		}
 		
 		if (atom->next) {
 			// adjust the sibling chain here: will probably require that atoms have a 'previous' pointer too, dammit!
 		}
 		
-		// must also adjust 'mdat' offset if it has moved (most likely!)
+		// here we adjust the chunk offsets for the media data, given in the 'stco' atom
+		uint32_t* stcoIPtr = (uint32_t*)stco->data;
+		uint32_t numOffsets = ntohl(*(stcoIPtr + 1));		// first byte of stco is ver num and flags; ignore
+		uint32_t* curPtr = stcoIPtr + 2;
+		uint32_t atomLen = atom->length;
+		
+		for (; numOffsets; numOffsets--, curPtr++)
+			*curPtr = htonl(ntohl(*curPtr) - atomLen);
 		
 		// delete the memory for the atom, because it is no longer in the tree
 		freeMPEG4Atom(atom);
